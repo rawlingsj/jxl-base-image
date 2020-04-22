d4456a5662ccebd131065b8ab625d9d8ce130e06
James Strachan <james.strachan@gmail.com>
2020-04-22 11:19:18 +0100

    fix: add auto promotion

--- promote.sh
+++ promote.sh
@@ -2,4 +2,4 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jxl-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jxl.git
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jxl-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jxl-base-image-jx.git
